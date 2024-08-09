8895a705c0a3f4a160a27632c9f7c081272e7665
David Mohr <david.mohr@ardexa.com>
2024-08-09 17:00:00 +1000

    fix: reduce open file limit to avoid erlang bug

--- create-cluster.sh
+++ create-cluster.sh
@@ -17,7 +17,7 @@ if [[ ! -f erlang-cookie.key ]]; then
 fi
 
 for i in 1 2 3; do
-    docker run -it -d --rm --name rabbit$i -h rabbit$i --net=rabbit-test -v $PWD/enabled_plugins:/etc/rabbitmq/enabled_plugins -v $PWD/erlang-cookie.key:/var/lib/rabbitmq/.erlang.cookie -p 567$i:5672 rabbitmq:$VERSION
+    docker run -it -d --rm --name rabbit$i -h rabbit$i --net=rabbit-test -v $PWD/enabled_plugins:/etc/rabbitmq/enabled_plugins -v $PWD/erlang-cookie.key:/var/lib/rabbitmq/.erlang.cookie -p 567$i:5672 --ulimit nofile=65536:65536 rabbitmq:$VERSION
 done
 
 docker exec rabbit2 bash -c 'rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@${HOSTNAME}.pid; rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbit1; rabbitmqctl start_app'
